5b4ab4d5d34c9cd22854356f3693e1f349192c1a
noba0 <noba35@outlook.fr>
2023-12-21 22:09:33 +0100

    Ajout d'une entité user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,6 +27,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $First_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Last_name = null;
+
+
+    #[ORM\Column]
+    private ?int $Phone_number = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -95,5 +105,41 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->First_name;
+    }
+
+    public function setFirstName(string $First_name): static
+    {
+        $this->First_name = $First_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->Last_name;
+    }
+
+    public function setLastName(string $Last_name): static
+    {
+        $this->Last_name = $Last_name;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?int
+    {
+        return $this->Phone_number;
+    }
+
+    public function setPhoneNumber(int $Phone_number): static
+    {
+        $this->Phone_number = $Phone_number;
+
+        return $this;
     }
 }

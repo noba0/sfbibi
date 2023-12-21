fd346f5bd6c630c2f5938b5f46702b6c10beef7e
noba0 <noba35@outlook.fr>
2023-12-21 21:19:43 +0100

    Ajout form user

---
+++ src/Form/BooksType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Books;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BooksType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Title')
+            ->add('Summary')
+            ->add('Edition')
+            ->add('Author')
+            ->add('Publication_year')
+            ->add('Image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Books::class,
+        ]);
+    }
+}
